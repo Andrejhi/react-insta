37b16ade61a16b4fb6ebc5f277bfdf3ee2afcc47
andru <room861@yandex.ru>
2019-12-09 20:08:28 +0300

    new

---
+++ precache-manifest.ee7322159f61f4a1b2472ed6f09c99e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e51763fb694ca70568c502078f5d0dcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b4eabb4e18c23554390",
+    "url": "/static/css/main.fcb48c17.chunk.css"
+  },
+  {
+    "revision": "5e5dba29de89ef9f5488",
+    "url": "/static/js/2.55cca482.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.55cca482.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b4eabb4e18c23554390",
+    "url": "/static/js/main.f92b57f9.chunk.js"
+  },
+  {
+    "revision": "a431f9be0f7710839dec",
+    "url": "/static/js/runtime-main.139dba7c.js"
+  },
+  {
+    "revision": "9e8a2b3937952b3de7aca04cb0d74020",
+    "url": "/static/media/logo.9e8a2b39.svg"
+  }
+]);
